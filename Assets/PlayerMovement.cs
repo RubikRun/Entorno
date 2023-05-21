c39e7d56ed0ad8fa3858845cbe0c0a5d42cc6945
Boris Dimitrov <borisd2000@gmail.com>
2023-05-21 13:27:07 +0300

    handle jumping of Player from user input

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     // Speed of horizontal movement
     private float speed = 6f;
+    private float jumpPower = 6f;
 
     private Rigidbody2D rigidBody;
 
@@ -21,5 +23,10 @@ public class PlayerMovement : MonoBehaviour
         // Handle horizontal movement
         float horizontalMove = Input.GetAxis("Horizontal");
         rigidBody.velocity = new Vector2(horizontalMove * speed, rigidBody.velocity.y);
+        // Handle jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpPower);
+        }
     }
 }
