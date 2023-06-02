a1368b88cfe64db97a092bba20b5f192e413137f
Boris Dimitrov <borisd2000@gmail.com>
2023-06-02 18:13:43 +0300

    add BearPopulator component to populate the maps with bears

---
+++ Assets/BearPopulator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearPopulator : MonoBehaviour
+{
+    GameObject bearOriginal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bearOriginal = GameObject.Find("BearOriginal");
+
+        const float distBetweenBears = 24f;
+
+        for (float x = distBetweenBears; x <= 50f; x += distBetweenBears)
+        {
+            createBear(x);
+        }
+        for (float x = -50f; x <= -distBetweenBears; x += distBetweenBears)
+        {
+            createBear(x);
+        }
+
+        GameObject.Destroy(bearOriginal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void createBear(float xPosition)
+    {
+        GameObject bearClone = Instantiate(bearOriginal, transform);
+        bearClone.name = "Bear" + xPosition.ToString();
+        bearClone.transform.localPosition = new Vector3(xPosition, bearOriginal.transform.localPosition.y, 0);
+    }
+}
